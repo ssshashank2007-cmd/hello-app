3d5c6971db1c3646f0f9ab96fa822a51be9cd011
ssshashank2007-cmd <ssshashank2007@gmial.com>
2026-04-10 13:56:11 +0530

    feature/uc5

--- src/main/java/org/example/uc5.java
+++ src/main/java/org/example/uc5.java
@@ -1,4 +1,22 @@
-package org.example;
-
 public class uc5 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder namesList = new StringBuilder();
+
+            int count = 0;
+            for (String name : args) {
+                namesList.append(name);
+
+                count++;
+                if (count < args.length) {
+                    namesList.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + namesList.toString() + "!");
+        }
+    }
 }
